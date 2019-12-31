f134a65b2b3747e1fac66c2e7a11143e7df2f9d4
Luke Bettridge <lukebettridge@hotmail.co.uk>
2019-12-31 15:10:04 +0000

    update catch all to exclude /api

--- server/init.prod.js
+++ server/init.prod.js
@@ -1,9 +1,9 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
 module.exports = app => {
-	app.use('/', express.static(process.cwd() + '/build'));
-	app.get('*', (_, res) => {
-		res.sendFile(path.resolve(__dirname, '../build/index.html'));
+	app.use("/", express.static(process.cwd() + "/build"));
+	app.get("/^/(?!api).*/", (_, res) => {
+		res.sendFile(path.resolve(__dirname, "../build/index.html"));
 	});
 };
