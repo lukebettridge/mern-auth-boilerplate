c8dafb9a10df96bb3f4ddd4519fde8d64177f0e0
Luke Bettridge <lukebettridge@hotmail.co.uk>
2020-03-01 17:30:32 +0000

    Implement isCreatable React select (#47)
    * Implement isCreatable select
    
    * Additional unit tests and update snapshot

--- __tests__/client/components/form/select/styles.test.jsx
+++ __tests__/client/components/form/select/styles.test.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { mount } from "enzyme";
 
-import { Select, SelectContainer } from "components/form/select/styles";
+import {
+	Container,
+	Select,
+	SelectContainer
+} from "components/form/select/styles";
 
 describe("Select styles", () => {
 	it("has value applied", () => {
-		const subject = mount(<Select value="foo" />);
+		const subject = mount(<SelectContainer value="foo" />);
 
-		expect(subject).toHaveStyleRule("font-size", "75%", {
+		expect(subject).toHaveStyleRule("font-size", "95%", {
 			modifier: "+ label"
 		});
 		expect(subject).toHaveStyleRule("transform", "translate3d(0,-100%,0)", {
@@ -19,24 +23,27 @@ describe("Select styles", () => {
 	});
 
 	it("in error applied", () => {
-		const subject = mount(<Select inError={true} />);
+		let subject;
 
+		subject = mount(<Select inError={true} />);
 		expect(subject).toHaveStyleRule("border-color", "#db1802", {
 			modifier: ".react-select__control"
 		});
+
+		subject = mount(<SelectContainer inError={true} />);
 		expect(subject).toHaveStyleRule("color", "#db1802", {
 			modifier: "+ label"
 		});
 	});
 
 	it("has label applied", () => {
-		const subject = mount(<SelectContainer label="foo" />);
+		const subject = mount(<Container label="foo" />);
 
 		expect(subject).toHaveStyleRule("margin-top", "30px");
 	});
 
 	it("bottom margin applied", () => {
-		const subject = mount(<SelectContainer mb />);
+		const subject = mount(<Container mb />);
 
 		expect(subject).toHaveStyleRule("margin-bottom", "30px");
 	});
