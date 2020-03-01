4b143769477ba1ed1b3a851a866659c2edbaeeb9
Luke Bettridge <lukebettridge@hotmail.co.uk>
2020-03-01 00:00:54 +0000

    Date picker unit tests

--- jest.config.js
+++ jest.config.js
@@ -22,5 +22,10 @@ module.exports = {
 			lines: 50,
 			statements: 50
 		}
+	},
+	moduleNameMapper: {
+		"\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+			"<rootDir>/__mocks__/file.js",
+		"\\.(css|scss|less)$": "<rootDir>/__mocks__/style.js"
 	}
 };
