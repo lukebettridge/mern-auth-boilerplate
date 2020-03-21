c735e54713b40f95f3f1bf5a20ce03c9f9a3968d
Luke Bettridge <lukebettridge@hotmail.co.uk>
2020-03-21 17:26:44 +0000

    Update dependencies

--- server/init.dev.js
+++ server/init.dev.js
@@ -2,17 +2,10 @@ const cors = require("cors");
 const webpack = require("webpack");
 const config = require("../webpack.config");
 const webpackDevMiddleware = require("webpack-dev-middleware");
-const { graphiqlExpress } = require("graphql-server-express");
 
 const compiler = webpack(config);
 
 module.exports = app => {
 	app.use(cors({ origin: true, credentials: true }));
-	app.use(
-		"/graphiql",
-		graphiqlExpress({
-			endpointURL: "/graphql"
-		})
-	);
 	app.use(webpackDevMiddleware(compiler));
 };
